d7586d9253252c179ecb1110ac01add031051b38
Michael Ammann <mazgch@users.noreply.github.com>
2018-01-23 10:58:43 +0100

    Update "hero" color also in translate menu

--- js/donut.js
+++ js/donut.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function () {
 
  	function changeGoogleStyles() { 		
 		if(($goog = $('.goog-te-menu-frame').contents().find('body')).length) {
-			var stylesHtml = '<style type="text/css">.goog-te-menu2-item, .goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2-item-selected{color: #ff7675; text-decoration: none;}.goog-te-menu2-item div{padding: 7px 10px !important;}, .goog-te-menu2 {border:0px !important;}.goog-te-menu2-item:hover div{color:#ffffff;background:#ff7675}.goog-te-menu2-item-selected div,.goog-te-menu2-item-selected:link div,.goog-te-menu2-item-selected:visited div,.goog-te-menu2-item-selected:hover div,.goog-te-menu2-item-selected:active div{color: #ffffff;background-color: #ff7675 !important;}.goog-te-menu2{border:0px!important;}</style>';
+			var stylesHtml = '<style type="text/css">.goog-te-menu2-item, .goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2-item-selected{color: #ff6e59; text-decoration: none;}.goog-te-menu2-item div{padding: 7px 10px !important;}, .goog-te-menu2 {border:0px !important;}.goog-te-menu2-item:hover div{color:#ffffff;background:#ff6e59}.goog-te-menu2-item-selected div,.goog-te-menu2-item-selected:link div,.goog-te-menu2-item-selected:visited div,.goog-te-menu2-item-selected:hover div,.goog-te-menu2-item-selected:active div{color: #ffffff;background-color: #ff6e59 !important;}.goog-te-menu2{border:0px!important;}</style>';
 
 
 			$goog.prepend(stylesHtml);
